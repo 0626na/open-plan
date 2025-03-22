8b6580e2bf974c8c260ee36e05c8714141ca9a03
SUKJIN HAN <0626na@gmail.com>
2025-03-22 17:55:59 +0900

    feat(web/result/components): Card 컴포넌트 생성

---
+++ apps/web/app/result/components/Card.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function Card({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex flex-col gap-4 md:flex-row md:gap-0 w-full justify-between bg-white p-4 rounded-xl shadow-sm">
+      {children}
+    </div>
+  );
+}
