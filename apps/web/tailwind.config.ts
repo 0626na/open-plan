9b50145dc802a9fdc0ac5b499bbd0e1bc49cb049
SUKJIN HAN <0626na@gmail.com>
2025-03-23 05:38:59 +0900

    chore(web): package/ui 경로 절대경로로 설정

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,9 +2,13 @@
 
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
+import path from "path";
 
 const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx", "../../packages/ui/src/**/*.{ts,tsx,js,jsx}"],
+  content: [
+    "./app/**/*.tsx",
+    path.join(__dirname, "../../packages/ui/src/**/*.{ts,tsx,js,jsx}"),
+  ],
   presets: [sharedConfig],
 };
 
