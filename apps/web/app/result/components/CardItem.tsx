fb2831f9b7cc27c57c1d915be24925b0cfe598b1
SUKJIN HAN <0626na@gmail.com>
2025-03-23 04:35:06 +0900

    refactor(CardItem): 컴포넌트에 로딩 상태 분기 로직 추가
    - loading prop을 추가해 로딩 여부에 따라 InfoLoading 컴포넌트를 렌더링하도록 분기 처리
    - JSX 구조를 명확하게 분리하여 가독성 향상

--- apps/web/app/result/components/CardItem.tsx
+++ apps/web/app/result/components/CardItem.tsx
@@ -1,16 +1,24 @@
+import { InfoLoading } from "./loading";
+
 export default function CardItem({
   title,
   value,
   className,
+  loading,
 }: {
   title: string;
   value: string;
   className?: string;
+  loading: boolean;
 }) {
   return (
     <div className={className ?? ""}>
       <p className="text-sm font-bold text-black">{title}</p>
-      <p className="text-black text-opacity-50">{value}</p>
+      {loading ? (
+        <InfoLoading />
+      ) : (
+        <p className="text-black text-opacity-50">{value}</p>
+      )}
     </div>
   );
 }
